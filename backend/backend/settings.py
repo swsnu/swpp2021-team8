f0cb9700224a1fd7ab05885f5313a01f4f8c149d
arclic <jy.heo1125@gmail.com>
2021-11-08 00:42:25 +0900

    Restore Django SECRET_KEY for test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = "django-insecure-a)wwuk0mog7lwko_4fc6k==(q74i7f51h%ce71$4z^uwhw-(qt"
+#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
