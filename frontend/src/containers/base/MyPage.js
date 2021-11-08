021fc42ffdf368913fbd89360d76ac879874f410
Chiwon Choi <ccw9615@gmail.com>
2021-11-08 21:04:10 +0900

    Issue#7 adjust mypage classnames

--- frontend/src/containers/base/MyPage.js
+++ frontend/src/containers/base/MyPage.js
@@ -8,10 +8,10 @@ import './MyPage.scss';
 const MyPage = () => {
   return (
     <>
-      <div className="my_info">
+      <div className="mypage__info">
         <h1> My Page </h1>
-        <div className="my_info__email">email</div>
-        <div className="my_info__username">username</div>
+        <div className="mypage__info__email">email</div>
+        <div className="mypage__info__username">username</div>
       </div>
       <GroupList />
       <Calendar />
