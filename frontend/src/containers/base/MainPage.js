d1b0559f7bc92ed26a6e6e3f7b40359af0724cf5
arclic <jy.heo1125@gmail.com>
2021-11-10 14:11:16 +0900

    Issue 17: Implement Group tab in MainPage

--- frontend/src/containers/base/MainPage.js
+++ frontend/src/containers/base/MainPage.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import GroupSearch from '../../components/group/GroupSearch';
-import GroupList from '../../components/group/GroupList';
-import ContentSearch from '../../components/content/ContentSearch';
+import { useSelector } from 'react-redux';
+import { FaFilter } from 'react-icons/fa';
+import GroupListItem from '../../components/group/GroupListItem';
 import ContentList from '../../components/content/ContentList';
+import './MainPage.scss';
 
 const MainPage = ({ history }) => {
   const [tab, setTab] = useState('group');
+
+  const groups = useSelector((state) => state.group.groups);
 
   const onGroupTabClick = () => {
     setTab('group');
@@ -18,33 +21,110 @@ const MainPage = ({ history }) => {
   const onCreateGroupClick = () => {
     history.push('/group/create');
   };
+
+  const onGroupSearchClick = () => {};
+  const onContentSearchClick = () => {};
+
   return (
     <>
-      <button id="group-tab" onClick={onGroupTabClick} type="button">
-        Group Tab
-      </button>
-      <button id="content-tab" onClick={onContentTabClick} type="button">
-        Content Tab
-      </button>
-      {tab === 'group' ? (
-        <>
-          <button
-            id="create-group-button"
-            onClick={onCreateGroupClick}
-            type="button"
+      <div className="main">
+        <div className="main__header">
+          <div
+            className={`main__header--tab ${tab === 'group' ? 'active' : ''}`}
+            onClick={onGroupTabClick}
+            role="button"
+            tabIndex={0}
           >
-            Create Group
-          </button>
-          <GroupSearch />
-          <GroupList />
-        </>
-      ) : (
-        <>
-          <ContentSearch />
-          <ContentList />
-          <ContentList />
-        </>
-      )}
+            Group
+          </div>
+          <div
+            className={`main__header--tab ${tab === 'content' ? 'active' : ''}`}
+            onClick={onContentTabClick}
+            role="button"
+            tabIndex={0}
+          >
+            Content
+          </div>
+          <div className="main__header--placeholder" />
+        </div>
+        {tab === 'group' ? (
+          <>
+            <div className="main__group-header">
+              <div className="main__search">
+                <input
+                  id="group-search-input"
+                  name="title"
+                  placeholder="Search group title / creator"
+                />
+                <button
+                  id="group-filter-button"
+                  type="button"
+                  className="main__search--filter"
+                >
+                  <FaFilter />
+                </button>
+                <button
+                  id="group-search-button"
+                  className="main__search--search"
+                  onClick={onGroupSearchClick}
+                  type="button"
+                >
+                  Search
+                </button>
+              </div>
+              <button
+                id="create-group-button"
+                onClick={onCreateGroupClick}
+                type="button"
+              >
+                Create Group
+              </button>
+            </div>
+
+            <div className="main__group-body">
+              <div className="main__group-body--header">
+                <div className="group-item__platform">Platform</div>
+                <div className="group-item__title">Title / Creator</div>
+                <div className="group-item__membership">Membership / Price</div>
+                <div className="group-item__member">Members</div>
+                <div className="group-item__duration">Duration</div>
+              </div>
+              {groups.map((group) => {
+                return <GroupListItem group={group} key={group.title} />;
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="main__content-header">
+              <div className="main__search">
+                <input
+                  id="content-search-input"
+                  name="title"
+                  placeholder="Search movie title"
+                />
+                <button
+                  id="content-filter-button"
+                  type="button"
+                  className="main__search--filter"
+                >
+                  <FaFilter />
+                </button>
+                <button
+                  id="content-search-button"
+                  className="main__search--search"
+                  onClick={onContentSearchClick}
+                  type="button"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+            <ContentList />
+            <ContentList />
+          </>
+        )}
+      </div>
     </>
   );
 };
--- frontend/src/containers/base/MyPage.js
+++ frontend/src/containers/base/MyPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import GroupList from '../../components/group/GroupList';
 import Calendar from '../../components/base/Calendar';
 import ContentList from '../../components/content/ContentList';
 
 const MyPage = () => {
   return (
     <>
-      <GroupList />
       <Calendar />
       <ContentList />
     </>
