c8a3b69a489f68e73d558818edac9c099d257105
arclic <jy.heo1125@gmail.com>
2021-11-10 20:01:10 +0900

    Issue 17: Implement Group/Content tab in MainPage

--- frontend/src/containers/base/MainPage.js
+++ frontend/src/containers/base/MainPage.js
@@ -2,20 +2,32 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { FaFilter } from 'react-icons/fa';
 import GroupListItem from '../../components/group/GroupListItem';
-import ContentList from '../../components/content/ContentList';
+import ContentListItem from '../../components/content/ContentListItem';
 import './MainPage.scss';
 
 const MainPage = ({ history }) => {
-  const [tab, setTab] = useState('group');
+  const [tab, setTab] = useState(
+    localStorage.getItem('mainTab') ? localStorage.getItem('mainTab') : 'group',
+  );
+  const [visibility, setVisibility] = useState(false);
 
   const groups = useSelector((state) => state.group.groups);
+  const recommendationContents = useSelector(
+    (state) => state.content.recommendationContents,
+  );
+
+  const trendingContents = useSelector(
+    (state) => state.content.trendingContents,
+  );
 
   const onGroupTabClick = () => {
     setTab('group');
+    localStorage.setItem('mainTab', 'group');
   };
 
   const onContentTabClick = () => {
     setTab('content');
+    localStorage.setItem('mainTab', 'content');
   };
 
   const onCreateGroupClick = () => {
@@ -24,6 +36,9 @@ const MainPage = ({ history }) => {
 
   const onGroupSearchClick = () => {};
   const onContentSearchClick = () => {};
+  const onFilterButtonClick = () => {
+    setVisibility(!visibility);
+  };
 
   return (
     <>
@@ -60,6 +75,7 @@ const MainPage = ({ history }) => {
                   id="group-filter-button"
                   type="button"
                   className="main__search--filter"
+                  onClick={onFilterButtonClick}
                 >
                   <FaFilter />
                 </button>
@@ -80,6 +96,67 @@ const MainPage = ({ history }) => {
                 Create Group
               </button>
             </div>
+            <div
+              className={`main__group-filter ${visibility ? 'visible' : ''}`}
+            >
+              <div className="main__group-filter--ott">
+                <div className="main__group-filter--ott-title">
+                  OTT Platform / Membership
+                </div>
+                <div className="main__group-filter--ott-option">
+                  <img
+                    src="/images/netflix.png"
+                    alt="netflix"
+                    height="40"
+                    width="40"
+                  />
+                  <div className="main__group-filter--button">Basic</div>
+                  <div className="main__group-filter--button">Premium</div>
+                </div>
+                <div className="main__group-filter--ott-option">
+                  <img
+                    src="/images/watcha.png"
+                    alt="watcha"
+                    height="40"
+                    width="40"
+                  />
+                  <div className="main__group-filter--button">Basic</div>
+                  <div className="main__group-filter--button">Standard</div>
+                  <div className="main__group-filter--button">Premium</div>
+                </div>
+                <div className="main__group-filter--ott-option">
+                  <img
+                    src="/images/tving.png"
+                    alt="tving"
+                    height="40"
+                    width="40"
+                  />
+                  <div className="main__group-filter--button">Basic</div>
+                </div>
+              </div>
+              <div className="main__group-filter--setting">
+                <div className="main__group-filter--column">
+                  <label htmlFor="filter-duration">Duration</label>
+                  <select id="filter-duration" name="filter-duration">
+                    duration
+                  </select>
+                </div>
+                <div className="main__group-filter--member">
+                  <div className="main__group-filter--column">
+                    <label htmlFor="sits-left">Sits Left</label>
+                    <select id="sits-left" name="sits-left">
+                      Sits left
+                    </select>
+                  </div>
+                  <div className="main__group-filter--column">
+                    <label htmlFor="total-member">Total Member</label>
+                    <select id="total-member" name="total-member">
+                      Total member
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             <div className="main__group-body">
               <div className="main__group-body--header">
@@ -90,7 +167,7 @@ const MainPage = ({ history }) => {
                 <div className="group-item__duration">Duration</div>
               </div>
               {groups.map((group) => {
-                return <GroupListItem group={group} key={group.title} />;
+                return <GroupListItem group={group} key={group.id} />;
               })}
             </div>
           </>
@@ -104,13 +181,6 @@ const MainPage = ({ history }) => {
                   placeholder="Search movie title"
                 />
                 <button
-                  id="content-filter-button"
-                  type="button"
-                  className="main__search--filter"
-                >
-                  <FaFilter />
-                </button>
-                <button
                   id="content-search-button"
                   className="main__search--search"
                   onClick={onContentSearchClick}
@@ -120,8 +190,24 @@ const MainPage = ({ history }) => {
                 </button>
               </div>
             </div>
-            <ContentList />
-            <ContentList />
+            <div className="main__content-body">
+              <div className="main__content-body--title">
+                Recommendation Contents
+              </div>
+              <div className="main__content-body--poster">
+                {recommendationContents.slice(0, 4).map((content) => {
+                  return <ContentListItem content={content} key={content.id} />;
+                })}
+              </div>
+            </div>
+            <div className="main__content-body">
+              <div className="main__content-body--title">Trending Contents</div>
+              <div className="main__content-body--poster">
+                {trendingContents.slice(4, 8).map((content) => {
+                  return <ContentListItem content={content} key={content.id} />;
+                })}
+              </div>
+            </div>
           </>
         )}
       </div>
--- frontend/src/containers/base/MyPage.js
+++ frontend/src/containers/base/MyPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Calendar from '../../components/base/Calendar';
-import ContentList from '../../components/content/ContentList';
 
 const MyPage = () => {
   return (
     <>
       <Calendar />
-      <ContentList />
     </>
   );
 };
