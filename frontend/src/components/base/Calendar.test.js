0d2a6a5fc2c340a54bec274cd1c44a4a934985aa
Chiwon Choi <ccw9615@gmail.com>
2021-11-13 03:54:09 +0900

    Issue#7, #33 implement calendar test for mypage

--- frontend/src/components/base/Calendar.test.js
+++ frontend/src/components/base/Calendar.test.js
@@ -1,20 +1,27 @@
-// import React from 'react';
-// import { mount, shallow } from 'enzyme';
-// import Calendar from './Calendar';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Calendar from './Calendar';
 
-// describe('<Calendar />', () => {
-//   it('should render without error when user goes to my page ', () => {
-//     const component = shallow(<Calendar />);
-//     expect(component.find('.calendar').length).toBe(1);
-//   });
-//   it('should not change year when user clicks prev button when not on Jan', () => {
-//     const stubYearState = 2021;
-//     const stubMonthState = 11;
-//     React.useState = jest.fn()
-//         .mockReturnValueOnce([stubYearState, {}])
-//         .mockReturnValueOnce([stubMonthState, {}]);
-//     const component = mount(<Calendar />);
-//     const wrapper = component.find('#previous-button');
-//     wrapper.simulate('click');
-//     });
-// });
+describe('<Calendar />', () => {
+  it('should render without error when user goes to my page ', () => {
+    const component = shallow(<Calendar />);
+    expect(component.find('.calendar').length).toBe(1);
+  });
+  it('should change date when user clicks prev and next button', () => {
+    const component = mount(<Calendar />);
+    component.find('#next-button').simulate('click');
+    component.find('#next-button').simulate('click');
+    expect(component.find('.calendar__header').text()).toBe('2022.1');
+    component.find('#previous-button').simulate('click');
+    expect(component.find('.calendar__header').text()).toBe('2021.12');
+    component.find('#previous-button').simulate('click');
+    expect(component.find('.calendar__header').text()).toBe('2021.11');
+  });
+  
+  it('should render today well', () => {
+    const component = mount(<Calendar />);
+    expect(component.find('.cell .today').length).toBe(1);
+    component.find('#next-button').simulate('click');
+    expect(component.find('.today').length).toBe(0);
+  });
+});
