3ac12d2d4233123618fb4063095ae5ff6d4fb88a
나혜주 <hyejuna@nahyejuui-MacBook-Pro-2.local>
2021-11-16 20:00:46 +0900

    structures implemented

--- frontend/src/components/review/ReviewListItem.js
+++ frontend/src/components/review/ReviewListItem.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteReview, editReview } from '../../store/ReviewStore';
+import './ReviewListItem.scss';
 
 const ReviewListItem = () => {
+  const dispatch = useDispatch();
+  const [editDetail, setEditDetail] = useState('');
+
+  const review = {
+    id: 1,
+    content: 1,
+    detail: 'I love it. This was the best movie of my life!!',
+    user: 'swpp',
+    created_at: '2021-11-11',
+  };
+
   const onEditClick = () => {
-    // TODO
+    const changedDetail = prompt('Edit Review', review.detail);
+    if (changedDetail !== null) {
+      if (changedDetail === '') {
+        setEditDetail(changedDetail);
+      }
+      dispatch(editReview(review.id, editDetail));
+    }
   };
 
   const onDeleteClick = () => {
-    // TODO
+    dispatch(deleteReview(review.id));
   };
+
   return (
     <>
-      <p>user</p>
-      <p>rate</p>
-      <p>content</p>
-      <button id="edit-button" onClick={onEditClick} type="button">
-        Edit
-      </button>
-      <button id="delete-button" onClick={onDeleteClick} type="button">
-        Delete
-      </button>
+      <div className="review">
+        <div className="review__detail">
+          {review.detail}
+        </div>
+        <div className="review__user">
+          {review.user}
+        </div>
+        <div className="review__createdat">
+          {review.created_at}
+        </div>
+        <div className="buttons">
+          <button id="edit-button" onClick={onEditClick} type="button">
+            Edit
+          </button>
+          <button id="delete-button" onClick={onDeleteClick} type="button">
+            Delete
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/containers/content/ContentDetailPage.js
+++ frontend/src/containers/content/ContentDetailPage.js
@@ -1,11 +1,41 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+// import { useLocation } from 'react-router-dom';
 import ReviewList from '../../components/review/ReviewList';
+import { addFavoriteContent } from '../../store/ContentStore';
+import { createReview } from '../../store/ReviewStore';
+import './ContentDetailPage.scss';
+import posterTmp from './temp/best_offer.png';
 
 const ContentDetailPage = ({ history }) => {
-  const [content, setContent] = useState('');
+  // const location = useLocation();
+  // const path = location.pathname.split('/')[2];
+  const user1 = {
+    id: 1,
+    username: 'swpp',
+  };
+
+  const dispatch = useDispatch();
+
+  const [content, setContent] = useState({
+    id: 1,
+    the_movie_id: 1,
+    name: 'Best Offer',
+    description: 'A lonely art expert working for a mysterious and reclusive heiress finds not only her art worth examining.',
+    favorite_cnt: 0,
+    favorite_users: [],
+    poster: posterTmp,
+    category: 'Crime, Drama',
+    staff: 'Director | Giuseppe Tornatore\nWriter | Giuseppe Tornatore\nStars | Geoffrey, RushJim, SturgessSylvia Hoeks',
+  });
+  const [newComment, setnewComment] = useState('');
+
+  const gradientStyle = {
+    background: 'linear-gradient(#C99208 5%, #000000 60%)',
+  };
 
   const onReviewContentChange = (e) => {
-    setContent(e.target.value);
+    setnewComment(e.target.value);
   };
 
   const onBackClick = () => {
@@ -13,47 +43,95 @@ const ContentDetailPage = ({ history }) => {
   };
 
   const onFavoriteClick = () => {
-    // TODO
-  };
-
-  const onReviewRateClick = () => {
-    // TODO
+    setContent({
+      ...content,
+      favorite_users: [...content.favorite_users, user1],
+    });
+    dispatch(addFavoriteContent(user1.id, content.id));
   };
 
   const onCreateReviewClick = () => {
-    // TODO
+    const Review = {
+      content: content.id,
+      detail: newComment,
+      user: user1,
+    };
+    dispatch(createReview(content.id, Review));
+  };
+
+  const renderField = (category, detail) => {
+    const classname = 'contentdetail__field '.concat(category.toLowerCase());
+    return (
+      <div className={classname}>
+        <div className={category.toLowerCase().concat(' category')}>
+          {category}
+        </div>
+        <div className={category.toLowerCase().concat(' detail')}>
+          {detail}
+        </div>
+      </div>
+    );
   };
 
   return (
-    <>
+    <div className="contentdetail">
       <button id="back-button" onClick={onBackClick} type="button">
-        Back
+        &#9664; Back
       </button>
-      <button id="favorite-button" onClick={onFavoriteClick} type="button">
-        favorite
-      </button>
-      <button
-        id="new-review-rate-button"
-        onClick={onReviewRateClick}
-        type="button"
-      >
-        new-review-rate
-      </button>
-      <input
-        type="text"
-        id="new-review-content-input"
-        value={content}
-        onChange={onReviewContentChange}
-      />
-      <button
-        id="create-review-button"
-        onClick={onCreateReviewClick}
-        type="button"
-      >
-        create-review
-      </button>
-      <ReviewList />
-    </>
+      <div className="contentdetail__main" style={gradientStyle}>
+        <div className="contentdetail__header">
+          <div className="contentdetail__left">
+            <img className="contentdetail__poster" src={content.poster} alt="poster" />
+          </div>
+          <div className="contentdetail__right">
+            <h1 className="contentdetail__name">
+              {content.name}
+            </h1>
+            <div className="contentdetail__favorite">
+              <button id="heart" onClick={onFavoriteClick} type="button" aria-label="favorite" />
+              <div className="contentdetail__favoritecount">
+                {content.favorite_cnt}
+              </div>
+            </div>
+            <div className="contentdetail__category">
+              {content.category}
+            </div>
+            <div className="contentdetail__staff">
+              {content.staff}
+            </div>
+          </div>
+        </div>
+        <div className="contentdetail__body">
+          {renderField('Description', content.description)}
+        </div>
+        <br />
+        <div className="contentdetail__footer">
+          <h1 className="contentdetail__name">
+            Review
+          </h1>
+          <br />
+          <div className="contentdetail__review">
+            <input
+              type="text"
+              id="new-review-content-input"
+              value={newComment}
+              onChange={onReviewContentChange}
+            />
+            <button
+              id="create-review-button"
+              onClick={onCreateReviewClick}
+              type="button"
+              disabled={newComment === ''}
+            >
+              write
+            </button>
+          </div>
+          <div className="contentdetail__reviewlist">
+            <ReviewList />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
