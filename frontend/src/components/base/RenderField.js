7936358db74e83ebae7c7c1b38ff042ebc2282b1
Chiwon Choi <ccw9615@gmail.com>
2021-11-25 20:53:34 +0900

    Issue56 starting point

---
+++ frontend/src/components/base/RenderField.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import './RenderField.scss';
+
+const RenderField = ({container, category, content, section}) => {
+  const classname = container.concat(
+    section ? '__'.concat(section) : '',
+    '__field ',
+    category.toLowerCase(),
+  );
+  return (
+    <div className={classname}>
+      <div className={category.toLowerCase().concat(' category')}>
+        {category}
+      </div>
+      <div className={category.toLowerCase().concat(' content')}>
+        {content}
+      </div>
+    </div>
+  );
+};
+export default withRouter(RenderField);
