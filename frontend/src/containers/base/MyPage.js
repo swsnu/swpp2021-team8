b25ea442b8065f6821af55b6ef02289e9dad5107
arclic <jy.heo1125@gmail.com>
2021-12-05 17:07:05 +0900

    Issue #84: Update MyPage info/group list

--- frontend/src/containers/base/MyPage.js
+++ frontend/src/containers/base/MyPage.js
@@ -1,65 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { useDispatch, useSelector } from 'react-redux';
 import Calendar from '../../components/base/Calendar';
 import GroupListItem from '../../components/group/GroupListItem';
+import { getGroups } from '../../store/GroupStore';
 import './MyPage.scss';
 
 const MyPage = () => {
-  const [groups] = useState(
-    [
-      {
-        id: 1,
-        platform: 'netflix',
-        title: 'Netflix Chillers',
-        leader: 'Netlover',
-        membership: 'Premium',
-        price: 5400,
-        curMember: 3,
-        maxMember: 4,
-        duration: 2,
-      },
-      {
-        id: 2,
-        platform: 'watcha',
-        title: 'Watcha Thriller Pot',
-        leader: 'swpp',
-        membership: 'Basic',
-        price: 1800,
-        curMember: 3,
-        maxMember: 4,
-        duration: 1,
-      },
-      {
-        id: 3,
-        platform: 'tving',
-        title: 'Tvinging',
-        leader: 'lion',
-        membership: 'Premium',
-        price: 4200,
-        curMember: 2,
-        maxMember: 4,
-        duration: 5,
-      },
-    ],
-  );
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const groups = useSelector((state) => state.group.groups);
+
+  // Group Pagination
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    dispatch(getGroups(`id=${user.id}`));
+  }, []);
+
+  useEffect(() => {
+    setPageCount(Math.ceil(groups.length / itemsPerPage));
+  }, [groups]);
+
+  // Invoke when user click to request another page.
+  const onHandlePageClick = (e) => {
+    const newOffset = (e.selected * itemsPerPage) % groups.length;
+    setItemOffset(newOffset);
+  };
+
   return (
     <div className="mypage">
       <div className="mypage__info">
         <h1> My Page </h1>
-        <div className="mypage__info__email">
-          <h2 className="category">email</h2>
-          <h2 className="content">swpp@snu.ac.kr</h2>
-        </div>
         <div className="mypage__info__username">
-          <h2 className="category">user name</h2>
-          <h2 className="content">iluvswpp</h2>
+          <h2 className="category">username</h2>
+          <h2 className="content">{user.username}</h2>
         </div>
       </div>
       <div className="mypage__content">
         <div className="mypage__grouplist">
           <h2>My Group</h2>
-          {groups.map((group) => {
-            return <GroupListItem group={group} key={group.id} />;
+          {groups.slice(itemOffset, itemOffset + itemsPerPage).map((group) => {
+            return <GroupListItem group={group} key={group.id} onlyTitle />;
           })}
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={onHandlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel="<"
+            renderOnZeroPageCount={null}
+          />
         </div>
         <hr />
         <div className="mypage__calendar">
