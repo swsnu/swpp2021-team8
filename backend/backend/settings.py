4201b902bfae317ace2ffa2ee2295df2cf7e12b5
arclic <jy.heo1125@gmail.com>
2021-12-10 02:01:39 +0900

    Isseu #105: Fix pylint error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import my_settings
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
