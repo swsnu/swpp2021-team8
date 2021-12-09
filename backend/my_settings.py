81ef3f71595f3845398d0deddb4d036f0fbe4e86
arclic <jy.heo1125@gmail.com>
2021-12-10 01:59:19 +0900

    Issue #105: Initialize DB

--- backend/my_settings.py
+++ backend/my_settings.py
@@ -1,2 +1,6 @@
 SECRET_KEY = "django-insecure-a)wwuk0mog7lwko_4fc6k==(q74i7f51h%ce71$4z^uwhw-(qt"
+<<<<<<< HEAD
 THE_MOVIE_API_KEY = "e50a928b29c2b7ed1f82ba2e6ce4982c"
+=======
+THE_MOVIE_API_KEY = "e50a928b29c2b7ed1f82ba2e6ce4982c"
+>>>>>>> df5e521 (Issue #105: Initialize DB)
