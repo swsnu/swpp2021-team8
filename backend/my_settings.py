cec06b8881bafb4937ae0c0be9566a45ae34a039
arclic <jy.heo1125@gmail.com>
2021-12-12 00:30:12 +0900

    Issue #115: Change db to mysql

--- backend/my_settings.py
+++ backend/my_settings.py
@@ -1,2 +1,12 @@
 SECRET_KEY = "django-insecure-a)wwuk0mog7lwko_4fc6k==(q74i7f51h%ce71$4z^uwhw-(qt"
 THE_MOVIE_API_KEY = "e50a928b29c2b7ed1f82ba2e6ce4982c"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'subroker',
+        'USER': 'root',
+        'PASSWORD': 'swppteam8',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
