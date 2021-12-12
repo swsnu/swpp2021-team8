242f17d37fe0c2e4d07247b0d54e9a81b2d343fe
arclic <jy.heo1125@gmail.com>
2021-12-12 23:31:37 +0900

    Issue #117: Add missing package depencencies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -81,14 +80,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = my_settings.DATABASES
+# DATABASES = my_settings.DATABASES
 
 
 # Password validation
